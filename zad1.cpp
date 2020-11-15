5d131782a489d05ecc84281c68947b409596a7d7
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-15 18:58:24 +0100

    delete after code to join with file in out struct

--- zad1.cpp
+++ zad1.cpp
@@ -8,15 +8,6 @@ using namespace std;
 
 int main(){
 
-vector<int> liczby(5,1);     // ilosc || wartosc * ilosc
-vector<string> nazwiska;
-
-for(auto e:liczby){
-    cout << e << endl;
-}
-
-
-
 
 
     return 0;
