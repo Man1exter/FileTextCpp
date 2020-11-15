0815fd22dc741796330cbe2d2503b5ad4705bf08
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-15 18:00:51 +0100

    training with vector

--- zad1.cpp
+++ zad1.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+
+
 int main(){
 
+vector<int> liczby(5,1);     // ilosc || wartosc * ilosc
+vector<string> nazwiska;
+
+for(auto e:liczby){
+    cout << e << endl;
+}
 
 
 
