de737104a461cfe3b722eb3d2527982612c4153e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-16 11:11:01 +0100

    done write name/s in file and save them

--- zad1.cpp
+++ zad1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -7,11 +8,23 @@ using namespace std;
 
 int main(){
 
+string imie,nazwisko;
 
 fstream plik;
 
-plik.open( "dane.txt", ios::out, ios::app );
+cout<< "Podaj swoje imie: " << endl;
+cin >> imie;
+cout << "Podaj swoje nazwisko: " << endl;
+cin >> nazwisko;
 
+plik.open( "dane.txt", ios::out | ios::app );
+
+plik << imie << endl;
+plik << nazwisko << endl;
+
+
+cout << "Dane zostaly zapisane!" << endl;
+plik.close();
 
     return 0;
 }
