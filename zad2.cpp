398e10758384699032d997535b4a5698634c7fe6
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-16 18:58:08 +0100

    comment text to exercises in last of two task/s

--- zad2.cpp
+++ zad2.cpp
@@ -14,3 +14,11 @@ int main(){
 
     return 0;
 }
+
+
+// Na podstawie przykładów przedstawionych na wykładzie napisz program zapisujący do ​plikubinarnego​ dane studenta. Każdy student będzie opisany takimi cechami jak:                                      Nazwisko, Imie,Nr_albumu, Wiek, Srednia_ocen.
+// Wykorzystaj typ złożony języka C++ ​Struct​.Program powinien zawierać trzy metody:
+
+//czytaj_dane_studenta_z_klawiatury​- odczytuje z klawiatury dane studenta
+//zapisz_dane_studenta_do_pliku​ - zapisuje strukturę do pliku binarnego
+//czytaj_dane_z_pliku​ - odczytuje dane z pliku binarnego i wyświetla na ekranie komputera
