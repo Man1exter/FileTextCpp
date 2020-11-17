ccb85e4289c7906a85dbeb7d8f15fe9d458c32b8
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-17 11:16:39 +0100

    create all elements with data od id person

--- zad2.cpp
+++ zad2.cpp
@@ -11,15 +11,46 @@ struct Osoba{
 
     string imie;
     string nazwisko;
-    int nrAlbumu
+    int nrAlbumu;
     float srOcen;
     int wiek;
-}
+};
 
 
 
 int main(){
 
+
+Osoba uczniowie[5];
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj imie i nazwisko " << i+1 << " ucznia: " ;
+    cin >> uczniowie[i].imie;
+    cin >> uczniowie[i].nazwisko;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj numer albumu " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].nrAlbumu;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj wiek " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].wiek;
+}
+
+cout << endl;
+
+for(int i = 0; i < 5; i++){
+    cout << "podaj srednia ocen: " << uczniowie[i].imie <<" "<< uczniowie[i].nazwisko <<" ";
+    cin >> uczniowie[i].wiek;
+}
+
+cout << endl;
 
 
 
