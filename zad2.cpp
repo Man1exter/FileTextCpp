d2c57c621088d3a5985a92beace80ce4f0f83f1a
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-17 10:58:49 +0100

    delete after version to create new with binary ctn

--- zad2.cpp
+++ zad2.cpp
@@ -7,66 +7,18 @@
 using namespace std;
 
 
-struct Student{
-    char imie[50];
-    char nazwisko[50];
-    int nrAlbumu;
-    int wiek;
+struct Osoba{
+
+    string imie;
+    string nazwisko;
+    int nrAlbumu
     float srOcen;
-};
+    int wiek;
+}
 
 
 
 int main(){
-
-
-
-Student dane;
-
-
-cout << "Podaj imie:" << endl;
-cin >> dane.imie;
-cout << "Podaj nazwisko:" << endl;
-cin >> dane.nazwisko;
-cout << "Podaj nrAlbumu:" << endl;
-cin >> dane.nrAlbumu;
-cout << "Podaj wiek:" << endl;
-cin >> dane.wiek;
-cout << "Podaj srednia ocen:" << endl;
-cin >> dane.srOcen;
-
-
-cout << "Student " << dane.imie  <<" "<< dane.nazwisko << " o numerze albumu: " << dane.nrAlbumu << " ma lat " << dane.wiek << " ma srednia ocen rowna: " << dane.srOcen << endl;
-
-
-
-ofstream ofs("dane.bin", ios::binary);
-
-Student* student = new Student;
-
-student->imie;
-student->nazwisko;
-student->nrAlbumu;
-student->wiek;
-student->srOcen;
-
-ofs.write((char*)(student), sizeof(Student));
-ofs.close();
-
- delete student;
-
-
-
-ifstream ifs("dane.bin", ios::binary);
-
-char* temp = new char[sizeof(Student)];
-ifs.read(temp, sizeof(Student));
-
-Student* student2 = (Student*)(temp);
-
-cout << "Student " << dane.imie  <<" "<< dane.nazwisko << " o numerze albumu: " << dane.nrAlbumu << " oraz ma lat " << dane.wiek << " ma srednia ocen rowna: " << dane.srOcen <<"  --------Potwierdzenie do zapisu i odczytu!-------" << endl;
-
-delete student;
 
 
 
